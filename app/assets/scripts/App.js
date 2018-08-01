20aade561fb4db2ea33cebb22f2fd7db9c85993c
sumithalder <sumithalder100@gmail.com>
2018-08-02 02:43:50 +0530

    Completed the scrolling effect

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import Mobilemenu from './modules/MoblieMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+
 
 var mobileMenu = new Mobilemenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial'), "65%");
+var stickyHeader = new StickyHeader();
