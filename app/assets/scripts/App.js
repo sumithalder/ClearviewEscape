9a2469f73b7073fe439194aa69a6130c92df7f93
sumithalder <sumithalder100@gmail.com>
2018-08-02 14:08:38 +0530

    Completed the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import Mobilemenu from './modules/MoblieMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
 var mobileMenu = new Mobilemenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial'), "65%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
